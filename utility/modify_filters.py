a26a77b6f4b8edffcea2f7b6585a8df206840e95
Evan Woods <22337004+efwoods@users.noreply.github.com>
2024-08-03 17:01:32 -0400

    plot the magnitude of the freqency; attempting to correctly create a filter for the frequency; current problem is the ifft of the frequency is mirrored about the x-axis; seeking a solution

---
+++ utility/modify_filters.py
@@ -0,0 +1,15 @@
+"""This module is used to modify filters & remove the noise of the
+raw data."""
+
+# Imports
+import sys
+import os
+
+
+
+def main(*args, **kwargs):
+    """This is the main driver of the module.
+    """
+
+if __name__ == '__main__':
+    main()
