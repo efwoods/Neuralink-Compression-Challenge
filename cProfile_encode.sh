aec21d117e6acd369de5d0a18b1b31333314ff04
Evan Woods <22337004+efwoods@users.noreply.github.com>
2024-10-12 23:45:06 -0400

    updated code to properly use joins; code now functions properly and reduces the original time to compute create_byte_string by 3 milliseconds

--- cProfile_encode.sh
+++ cProfile_encode.sh
@@ -1,1 +1,1 @@
-python -m cProfile -s tottime encode data/0052503c-2849-4f41-ab51-db382103690c.wav data/0052503c-2849-4f41-ab51-db382103690c.wav.brainwire >> encode_updated.txt
+python -m cProfile -s tottime encode data/0052503c-2849-4f41-ab51-db382103690c.wav data/0052503c-2849-4f41-ab51-db382103690c.wav.brainwire >> encode_updated2.txt
