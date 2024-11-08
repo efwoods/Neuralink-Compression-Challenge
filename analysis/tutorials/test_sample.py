2a88a81d36dc6e76c8ae507f33be1bab926e658f
Evan Woods <22337004+efwoods@users.noreply.github.com>
2024-11-08 16:21:04 -0500

    updating test_sample to allow the test to pass with remote github workflow

--- analysis/tutorials/test_sample.py
+++ analysis/tutorials/test_sample.py
@@ -1,6 +1,9 @@
 # Pytest Example
+
+# A sample function:
 def inc(x):
     return x + 1
 
-def test_answer():
-    assert inc(3) == 5
+# Test that is designed to fail:
+# def test_answer():
+#     assert inc(3) == 5
